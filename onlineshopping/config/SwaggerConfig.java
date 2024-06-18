42e87d6b1585909fd6014cdf9aafcbe56c098695
Hakeema-26 <125954386+Hakeema-26@users.noreply.github.com>
2024-06-18 16:26:37 +0530

    Update SwaggerConfig.java

--- onlineshopping/config/SwaggerConfig.java
+++ onlineshopping/config/SwaggerConfig.java
@@ -1,9 +1,5 @@
 package com.batch2.onlineshopping.config;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
 
 @OpenAPIDefinition(
 		info = @Info(
