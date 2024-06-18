a5a63f8498198523a7a0d12f4a4f1ae47fcf15d1
Hakeema-26 <125954386+Hakeema-26@users.noreply.github.com>
2024-06-18 16:25:10 +0530

    Delete onlineshopping/exceptions directory

--- onlineshopping/exceptions/InvalidProductDetailsException.java
+++
@@ -1,9 +0,0 @@
-package com.batch2.onlineshopping.exceptions;
-
-public class InvalidProductDetailsException extends Exception {
-
-	public InvalidProductDetailsException(String message) {
-		super(message);
-		// TODO Auto-generated constructor stub
-	}
-}
--- onlineshopping/exceptions/InvalidUserDetailsException.java
+++
@@ -1,9 +0,0 @@
-package com.batch2.onlineshopping.exceptions;
-
-public class InvalidUserDetailsException extends Exception {
-
-	public InvalidUserDetailsException(String message) {
-		super(message);
-	}
-
-}
