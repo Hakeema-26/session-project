6242a8250f7cbfe4997f0801d8f9b4535bee719d
Haripappula12 <163239973+Haripappula12@users.noreply.github.com>
2024-06-21 19:42:06 +0530

    Update CartController.java

--- onlineshopping/controller/CartController.java
+++ onlineshopping/controller/CartController.java
@@ -1,14 +1,6 @@
 package com.batch2.onlineshopping.controller;
 
-import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 
 
