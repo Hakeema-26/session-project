80c91e63bc5f9e8d6a646cc55aa953e3220ca655
Haripappula12 <163239973+Haripappula12@users.noreply.github.com>
2024-06-21 18:59:34 +0530

    Update AuthController.java

--- onlineshopping/controller/AuthController.java
+++ onlineshopping/controller/AuthController.java
@@ -10,8 +10,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
 
 import com.batch2.onlineshopping.config.JwtUtils;
 import com.batch2.onlineshopping.entity.LoginRequest;
